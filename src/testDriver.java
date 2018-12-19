3c727bea20a77e9fd225bcdbfa916328fff8fc67
Kwestion Ahmad <ahmad50017@mvctc.com>
2018-12-19 13:28:14 -0500

    created the ascii title and started an instructions function

--- src/testDriver.java
+++ src/testDriver.java
@@ -8,6 +8,12 @@ import java.util.Scanner;
 public class testDriver {
 
     public static void main(String[] args) {
+        
+        displayTitle();
+        System.out.println();
+        System.out.println("The Void by Kwestion Ahmad");
+        System.out.println();
+        
         Scanner in = new Scanner(System.in);
         Player mainP;
         Ninja n1;
@@ -109,5 +115,34 @@ public class testDriver {
             
         }
         
+    }
+    
+    public static void displayTitle(){
+        System.out.println("    .....                                                                        .       ..      "); 
+        System.out.println(" .H8888888h.  ~-.    .uef^\"                                                     @88>   dF        "); 
+        System.out.println(" 888888888888x  `> :d88E                          ...     ..             u.     %8P   '88bu.     "); 
+        System.out.println("X~     `?888888hx~ `888E            .u         :~\"\"888h.:^\"888:    ...ue888b     .    '*88888bu  "); 
+        System.out.println("'      x8.^\"*88*\"   888E .z8k    ud8888.      8X   `8888X  8888>   888R Y888r  .@88u    ^\"*8888N "); 
+        System.out.println(" `-:- X8888x        888E~?888L :888'8888.    X888n. 8888X  ?888>   888R I888> ''888E`  beWE \"888L"); 
+        System.out.println("      488888>       888E  888E d888 '88%\"    '88888 8888X   ?**h.  888R I888>   888E   888E  888E"); 
+        System.out.println("    .. `\"88*        888E  888E 8888.+\"         `*88 8888~ x88x.    888R I888>   888E   888E  888E"); 
+        System.out.println("  x88888nX\"      .  888E  888E 8888L          ..<\"  88*`  88888X  u8888cJ888    888E   888E  888F"); 
+        System.out.println(" !\"*8888888n..  :   888E  888E '8888c. .+        ..XC.    `*8888k  \"*888*P\"     888&  .888N..888 "); 
+        System.out.println("'    \"*88888888*   m888N= 888>  \"88888%        :888888H.    `%88>    'Y\"        R888\"  `\"888*\"\"  "); 
+        System.out.println("        ^\"***\"`     `Y\"   888     \"YP'        <  `\"888888:    X\"                 \"\"       \"\"     "); 
+        System.out.println("                         J88\"                       %888888x.-`                                  "); 
+        System.out.println("                         @%                           \"\"**\"\"                                     "); 
+        System.out.println("                       :\"                                                                        "); 
+    }
+    
+    public static void displayInstructions(){
+        System.out.println();
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println();
     }
 }
