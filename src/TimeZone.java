c3ee67288a7e89c645af8c256a8023dfbdde5555
Kwestion Ahmad <ahmad50017@mvctc.com>
2019-03-05 12:37:10 -0500

    Finished Basic Movement
    You can move and interact with the timemachine

--- src/TimeZone.java
+++ src/TimeZone.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 public class TimeZone {
 
-    private int[][] map;
-    
+    public int myMap[][];
+
     private boolean hasEntered;
 
     private int timePeriod;
     private int year;
 
-    
-    TimeZone(int timePeriod){
+    TimeZone(int timePeriod) {
         this.timePeriod = timePeriod;
-        
-        switch(timePeriod)
-        {
+        myMap = new int[12][17];
+        this.initMap();
+
+        switch (timePeriod) {
             case 1:
                 this.year = 1396;
                 System.out.println("The year is: " + this.year);
@@ -35,10 +35,10 @@ public class TimeZone {
             case 5:
                 this.year = 3065;
                 System.out.println("The year is: " + this.year);
-                break;             
+                break;
         }
     }
-    
+
     public boolean setHasEntered(boolean entered) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
@@ -54,7 +54,7 @@ public class TimeZone {
     public int getTimePeriod() {
         return this.timePeriod;
     }
-    
+
     public int getYear() {
         return year;
     }
@@ -62,26 +62,31 @@ public class TimeZone {
     public void setYear(int year) {
         this.year = year;
     }
-    public void timeTravel(Scanner in){
+    
+    public int[][] getMyMap(){
+        return myMap;
+    }
+
+    public void timeTravel(Scanner in) {
         System.out.println("What year would you like to time travel to:");
         System.out.println("1) 1396");
         System.out.println("2) 1874");
         System.out.println("3) 2019");
         System.out.println("4) 2064");
         System.out.println("5) 3065");
-        
+
         int choice = 0;
         int currentTP = this.timePeriod;
-        
-        while (choice== 0 || choice == currentTP){
+
+        while (choice == 0 || choice == currentTP) {
             if (choice == currentTP) {
-                System.out.println("Sorry that is the year tou are current at. "
+                System.out.println("Sorry that is the year you are currently at. "
                         + "Please choose a different year.");
             }
             choice = in.nextInt();
         }
-        
-        switch(choice){
+
+        switch (choice) {
             case 1:
                 this.timePeriod = 1;
                 this.year = 1396;
@@ -102,9 +107,230 @@ public class TimeZone {
                 this.timePeriod = 5;
                 this.year = 3065;
                 break;
-                                   
+
         }
-        
+
         System.out.println("You have traveled to the year: " + this.year);
     }
+
+    private void initMap() {//16 x 9
+
+        this.myMap[0][0] = 4;
+        this.myMap[0][1] = 4;
+        this.myMap[0][2] = 4;
+        this.myMap[0][3] = 4;
+        this.myMap[0][4] = 4;
+        this.myMap[0][5] = 4;
+        this.myMap[0][6] = 4;
+        this.myMap[0][7] = 4;
+        this.myMap[0][8] = 4;
+        this.myMap[0][9] = 4;
+        this.myMap[0][10] = 4;
+        this.myMap[0][11] = 4;
+        this.myMap[0][12] = 4;
+        this.myMap[0][13] = 4;
+        this.myMap[0][14] = 4;
+        this.myMap[0][15] = 4;
+        this.myMap[0][16] = 4;
+
+        this.myMap[1][0] = 4;
+        this.myMap[1][1] = 8;
+        this.myMap[1][2] = 0;
+        this.myMap[1][3] = 0;
+        this.myMap[1][4] = 0;
+        this.myMap[1][5] = 0;
+        this.myMap[1][6] = 0;
+        this.myMap[1][7] = 0;
+        this.myMap[1][8] = 4;
+        this.myMap[1][9] = 4;
+        this.myMap[1][10] = 4;
+        this.myMap[1][11] = 4;
+        this.myMap[1][12] = 4;
+        this.myMap[1][13] = 4;
+        this.myMap[1][14] = 4;
+        this.myMap[1][15] = 4;
+        this.myMap[1][16] = 4;
+
+        this.myMap[2][0] = 4;
+        this.myMap[2][1] = 0;
+        this.myMap[2][2] = 0;
+        this.myMap[2][3] = 0;
+        this.myMap[2][4] = 0;
+        this.myMap[2][5] = 0;
+        this.myMap[2][6] = 0;
+        this.myMap[2][7] = 0;
+        this.myMap[2][8] = 4;
+        this.myMap[2][9] = 4;
+        this.myMap[2][10] = 4;
+        this.myMap[2][11] = 4;
+        this.myMap[2][12] = 4;
+        this.myMap[2][13] = 4;
+        this.myMap[2][14] = 4;
+        this.myMap[2][15] = 4;
+        this.myMap[2][16] = 4;
+
+        this.myMap[3][0] = 4;
+        this.myMap[3][1] = 0;
+        this.myMap[3][2] = 0;
+        this.myMap[3][3] = 3;
+        this.myMap[3][4] = 1;
+        this.myMap[3][5] = 0;
+        this.myMap[3][6] = 0;
+        this.myMap[3][7] = 0;
+        this.myMap[3][8] = 4;
+        this.myMap[3][9] = 4;
+        this.myMap[3][10] = 0;
+        this.myMap[3][11] = 0;
+        this.myMap[3][12] = 0;
+        this.myMap[3][13] = 0;
+        this.myMap[3][14] = 0;
+        this.myMap[3][15] = 0;
+        this.myMap[3][16] = 4;
+
+        this.myMap[4][0] = 4;
+        this.myMap[4][1] = 0;
+        this.myMap[4][2] = 0;
+        this.myMap[4][3] = 0;
+        this.myMap[4][4] = 0;
+        this.myMap[4][5] = 0;
+        this.myMap[4][6] = 0;
+        this.myMap[4][7] = 0;
+        this.myMap[4][8] = 4;
+        this.myMap[4][9] = 4;
+        this.myMap[4][10] = 0;
+        this.myMap[4][11] = 0;
+        this.myMap[4][12] = 0;
+        this.myMap[4][13] = 5;
+        this.myMap[4][14] = 0;
+        this.myMap[4][15] = 0;
+        this.myMap[4][16] = 4;
+
+        this.myMap[5][0] = 4;
+        this.myMap[5][1] = 0;
+        this.myMap[5][2] = 0;
+        this.myMap[5][3] = 0;
+        this.myMap[5][4] = 0;
+        this.myMap[5][5] = 0;
+        this.myMap[5][6] = 0;
+        this.myMap[5][7] = 0;
+        this.myMap[5][8] = 0;
+        this.myMap[5][9] = 0;
+        this.myMap[5][10] = 0;
+        this.myMap[5][11] = 0;
+        this.myMap[5][12] = 0;
+        this.myMap[5][13] = 0;
+        this.myMap[5][14] = 0;
+        this.myMap[5][15] = 0;
+        this.myMap[5][16] = 4;
+
+        this.myMap[6][0] = 4;
+        this.myMap[6][1] = 0;
+        this.myMap[6][2] = 0;
+        this.myMap[6][3] = 0;
+        this.myMap[6][4] = 0;
+        this.myMap[6][5] = 0;
+        this.myMap[6][6] = 0;
+        this.myMap[6][7] = 0;
+        this.myMap[6][8] = 0;
+        this.myMap[6][9] = 0;
+        this.myMap[6][10] = 0;
+        this.myMap[6][11] = 0;
+        this.myMap[6][12] = 0;
+        this.myMap[6][13] = 0;
+        this.myMap[6][14] = 0;
+        this.myMap[6][15] = 0;
+        this.myMap[6][16] = 4;
+
+        this.myMap[7][0] = 4;
+        this.myMap[7][1] = 0;
+        this.myMap[7][2] = 0;
+        this.myMap[7][3] = 0;
+        this.myMap[7][4] = 0;
+        this.myMap[7][5] = 0;
+        this.myMap[7][6] = 0;
+        this.myMap[7][7] = 0;
+        this.myMap[7][8] = 0;
+        this.myMap[7][9] = 0;
+        this.myMap[7][10] = 0;
+        this.myMap[7][11] = 0;
+        this.myMap[7][12] = 0;
+        this.myMap[7][13] = 0;
+        this.myMap[7][14] = 0;
+        this.myMap[7][15] = 0;
+        this.myMap[7][16] = 4;
+
+        this.myMap[8][0] = 4;
+        this.myMap[8][1] = 4;
+        this.myMap[8][2] = 4;
+        this.myMap[8][3] = 4;
+        this.myMap[8][4] = 4;
+        this.myMap[8][5] = 4;
+        this.myMap[8][6] = 4;
+        this.myMap[8][7] = 4;
+        this.myMap[8][8] = 4;
+        this.myMap[8][9] = 4;
+        this.myMap[8][10] = 0;
+        this.myMap[8][11] = 0;
+        this.myMap[8][12] = 0;
+        this.myMap[8][13] = 0;
+        this.myMap[8][14] = 0;
+        this.myMap[8][15] = 0;
+        this.myMap[8][16] = 4;
+
+        this.myMap[9][0] = 4;
+        this.myMap[9][1] = 4;
+        this.myMap[9][2] = 4;
+        this.myMap[9][3] = 4;
+        this.myMap[9][4] = 4;
+        this.myMap[9][5] = 4;
+        this.myMap[9][6] = 4;
+        this.myMap[9][7] = 4;
+        this.myMap[9][8] = 4;
+        this.myMap[9][9] = 4;
+        this.myMap[9][10] = 0;
+        this.myMap[9][11] = 0;
+        this.myMap[9][12] = 0;
+        this.myMap[9][13] = 0;
+        this.myMap[9][14] = 0;
+        this.myMap[9][15] = 0;
+        this.myMap[9][16] = 4;
+
+        this.myMap[10][0] = 4;
+        this.myMap[10][1] = 4;
+        this.myMap[10][2] = 4;
+        this.myMap[10][3] = 4;
+        this.myMap[10][4] = 4;
+        this.myMap[10][5] = 4;
+        this.myMap[10][6] = 4;
+        this.myMap[10][7] = 4;
+        this.myMap[10][8] = 4;
+        this.myMap[10][9] = 4;
+        this.myMap[10][10] = 0;
+        this.myMap[10][11] = 0;
+        this.myMap[10][12] = 0;
+        this.myMap[10][13] = 0;
+        this.myMap[10][14] = 0;
+        this.myMap[10][15] = 0;
+        this.myMap[10][16] = 4;
+
+        this.myMap[11][0] = 4;
+        this.myMap[11][1] = 4;
+        this.myMap[11][2] = 4;
+        this.myMap[11][3] = 4;
+        this.myMap[11][4] = 4;
+        this.myMap[11][5] = 4;
+        this.myMap[11][6] = 4;
+        this.myMap[11][7] = 4;
+        this.myMap[11][8] = 4;
+        this.myMap[11][9] = 4;
+        this.myMap[11][10] = 4;
+        this.myMap[11][11] = 4;
+        this.myMap[11][12] = 4;
+        this.myMap[11][13] = 4;
+        this.myMap[11][14] = 4;
+        this.myMap[11][15] = 4;
+        this.myMap[11][16] = 4;
+
+    }
+
 }
--- src/testDriver.java
+++ src/testDriver.java
@@ -9,197 +9,271 @@ import java.util.Scanner;
 public class testDriver {
 
     public static void main(String[] args) {
-        
-        displayTitle();
-        
-        
+
+        //displayTitle();
         Scanner in = new Scanner(System.in);
         Player mainP;
         Ninja n1;
         TimeZone current;
         current = new TimeZone(3);
         int choice = 1;
-        while (choice == 1){
-            current.timeTravel(in);
+        
+        mainP = new Player("John");
+        
+        while (choice == 1) {
             
-            System.out.println("hello");
-            choice = in.nextInt();
-        }
-        
-
-        
-
-        System.out.println("Would you like to 1) Create your own name, or "
-
-                + "2) Have a name generated for you");
-        choice = in.nextInt();
-        if (choice == 1) {
-            System.out.println("What would you like to name your character?");
             
-            mainP = new Player(in.next());
-        }else{
-            mainP = new Player("John");
-        }
-        
-
-        Dialog(mainP, in);
-        
-       
-        /*      TESTING THE FIGHTS
-        System.out.println("Would you like to 1) Create your own Henchman, or "
-                + "2) Have a ninja generated for you");
-        choice = in.nextInt();
-        if (choice == 1) {
-            n1 = new Ninja(NinjaType.HENCHMAN);
-        }else{
-            n1 = new Ninja();
+            System.out.println("Before.. X: "+ mainP.getXPos() + " Y: " + mainP.getYPos());
+            for (int[] x : current.getMyMap()) {
+                for (int y : x) {
+                    System.out.print(y + " ");
+                }
+                System.out.println();
+            }
+            move(in, current.myMap, mainP, current);
+            System.out.println("After.. X: "+ mainP.getXPos() + " Y: " + mainP.getYPos());
+                  
+            
         }
 
+//Mildred, Margaret, Mary, Mellisa, Mindie
+        
+        promptEnterKey();
         clear();
-        System.out.println("Main's Data");
-        mainP.printInfo();
-        
-        System.out.println("============================");
-        System.out.println("Ninja's Data");
-        n1.setName("Jacob");
-        n1.printInfo();
-        
-       fightHandler(n1, mainP);
-       clear();
-       System.out.println("out of fight\n\n");
-       System.out.println("Main's Data");
-        mainP.printInfo();
-        
-        System.out.println("============================");
-        System.out.println("Ninja's Data");
-        n1.printInfo();
-        */
-        clear();
-        
-    }
-    
-    public static void clear(){
-        for(int i = 0; i < 27; i++){
+
+    }//End of main
+
+    public static void clear() {
+        for (int i = 0; i < 27; i++) {
             System.out.print("\n");
         }
-    }
-    
-    public static void fightHandler(Ninja N, Player P){
+    }//End of clear
+
+    public static void fightHandler(Ninja N, Player P) {
         int choice;
         Scanner in = new Scanner(System.in);
         int attackBuffer;
         boolean counterAttack;
-        
-         while(N.getHealth() > 0 && P.getHealth() > 0 ){
+
+        while (N.getHealth() > 0 && P.getHealth() > 0) {
             clear();
             System.out.println("============================");
             System.out.println();
-            
+
             System.out.println("Would you like to: \n"
-                             + "1) Attack\n"
-                             + "2) Dodge\n"
-                             + "3) Nothing\n");
+                    + "1) Attack\n"
+                    + "2) Dodge\n"
+                    + "3) Nothing\n");
             choice = in.nextInt();
-            
-            switch(choice){
+
+            switch (choice) {
                 case 1:
-                   attackBuffer = P.attack();
-                   counterAttack = N.dodge(attackBuffer);
-                   if(counterAttack == true){
-                       P.takeDamage(N.attack() * 2);
-                   }
-                   break;
-                   
+                    attackBuffer = P.attack();
+                    counterAttack = N.dodge(attackBuffer);
+                    if (counterAttack == true) {
+                        P.takeDamage(N.attack() * 2);
+                    }
+                    break;
+
                 case 2:
                     attackBuffer = N.attack();
-                   counterAttack = P.dodge(attackBuffer);
-                    if(counterAttack == true){
-                       N.takeDamage(P.attack()* 2);
-                   }
+                    counterAttack = P.dodge(attackBuffer);
+                    if (counterAttack == true) {
+                        N.takeDamage(P.attack() * 2);
+                    }
                     break;
-                    
+
                 case 3:
                     attackBuffer = N.attack();
                     P.takeDamage(attackBuffer);
                     break;
             }
-            
+
             System.out.println("Main's Data");
             P.printInfo();
-        
+
             System.out.println("============================");
             System.out.println("Ninja's Data");
             N.printInfo();
-            
-            
-            
-        }
-        
-    }
 
-    
-    public static void displayTitle(){
-        System.out.println("    .....                                                                        .       ..      "); 
-        System.out.println(" .H8888888h.  ~-.    .uef^\"                                                     @88>   dF        "); 
-        System.out.println(" 888888888888x  `> :d88E                          ...     ..             u.     %8P   '88bu.     "); 
-        System.out.println("X~     `?888888hx~ `888E            .u         :~\"\"888h.:^\"888:    ...ue888b     .    '*88888bu  "); 
-        System.out.println("'      x8.^\"*88*\"   888E .z8k    ud8888.      8X   `8888X  8888>   888R Y888r  .@88u    ^\"*8888N "); 
-        System.out.println(" `-:- X8888x        888E~?888L :888'8888.    X888n. 8888X  ?888>   888R I888> ''888E`  beWE \"888L"); 
-        System.out.println("      488888>       888E  888E d888 '88%\"    '88888 8888X   ?**h.  888R I888>   888E   888E  888E"); 
-        System.out.println("    .. `\"88*        888E  888E 8888.+\"         `*88 8888~ x88x.    888R I888>   888E   888E  888E"); 
-        System.out.println("  x88888nX\"      .  888E  888E 8888L          ..<\"  88*`  88888X  u8888cJ888    888E   888E  888F"); 
-        System.out.println(" !\"*8888888n..  :   888E  888E '8888c. .+        ..XC.    `*8888k  \"*888*P\"     888&  .888N..888 "); 
-        System.out.println("'    \"*88888888*   m888N= 888>  \"88888%        :888888H.    `%88>    'Y\"        R888\"  `\"888*\"\"  "); 
-        System.out.println("        ^\"***\"`     `Y\"   888     \"YP'        <  `\"888888:    X\"                 \"\"       \"\"     "); 
-        System.out.println("                         J88\"                       %888888x.-`                                  "); 
-        System.out.println("                         @%                           \"\"**\"\"                                     "); 
-        System.out.println("                       :\"                                                                        "); 
+        }
+
+    }//End of fightHandler
+
+    public static void displayTitle() {
+        System.out.println("    .....                                         "
+                + "                              .       ..      ");
+
+        System.out.println(" .H8888888h.  ~-.    .uef^\"                      "
+                + "                               @88>   dF        ");
+
+        System.out.println(" 888888888888x  `> :d88E                          "
+                + "...     ..             u.     %8P   '88bu.     ");
+
+        System.out.println("X~     `?888888hx~ `888E            .u         :~"
+                + "\"\"888h.:^\"888:    ...ue888b     .    '*88888bu  ");
+
+        System.out.println("'      x8.^\"*88*\"   888E .z8k    ud8888.      8X"
+                + "   `8888X  8888>   888R Y888r  .@88u    ^\"*8888N ");
+
+        System.out.println(" `-:- X8888x        888E~?888L :888'8888.    X888n"
+                + ". 8888X  ?888>   888R I888> ''888E`  beWE \"888L");
+
+        System.out.println("      488888>       888E  888E d888 '88%\"    '888"
+                + "88 8888X   ?**h.  888R I888>   888E   888E  888E");
+
+        System.out.println("    .. `\"88*        888E  888E 8888.+\"         `"
+                + "*88 8888~ x88x.    888R I888>   888E   888E  888E");
+
+        System.out.println("  x88888nX\"      .  888E  888E 8888L          ..<"
+                + "\"  88*`  88888X  u8888cJ888    888E   888E  888F");
+
+        System.out.println(" !\"*8888888n..  :   888E  888E '8888c. .+        "
+                + "..XC.    `*8888k  \"*888*P\"     888&  .888N..888 ");
+
+        System.out.println("'    \"*88888888*   m888N= 888>  \"88888%        :"
+                + "888888H.    `%88>    'Y\"        R888\"  `\"888*\"\"  ");
+
+        System.out.println("        ^\"***\"`     `Y\"   888     \"YP'        "
+                + "<  `\"888888:    X\"                 \"\"       \"\"     ");
+
+        System.out.println("                         J88\"                    "
+                + "   %888888x.-`                                  ");
+
+        System.out.println("                         @%                       "
+                + "    \"\"**\"\"                                     ");
+
+        System.out.println("                       :\"                        "
+                + "                                                ");
+
         System.out.println();
         System.out.println("The Void by Kwestion Ahmad and Hunter Stewart");
         System.out.println();
-    }
-    
-    public static void displayInstructions(){
-        System.out.println();
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println();
-    }
-}
+    }//End of displayTItle
 
-    public static void Dialog(Player p, Scanner in){
+    public static void displayInstructions() {
+        System.out.println();
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println();
+    }//End of displayInstructions
+
+    public static void Dialog(Player p, Scanner in) {
         System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------- ");
-        System.out.println("Year: 2019\n" +
-                           "Location: Cam Coorperation\n" +
-                           "Date: January 3rd\n" +
-                           "\n" +
-                           "Hello. How have you been? Working for \"Cam-Corp\" is still fun. It has been great here. People are very nice here and I have fit in really good in here.\n" +
-                           "Although, I have a feeling something else is happening here.. People keep going into this inclosed room next to my table. I can not see what it is but\n" +
-                           "I keep seeing bright lights come into the room at different times... Weird lights. Ones I have never seen before... Anyways I hope you are \n" +
-                           "having fun! I will see you soon. Love, " + p.getName());
+        System.out.println("Year: 2019\n"
+                + "Location: Cam Coorperation\n"
+                + "Date: January 3rd\n"
+                + "\n"
+                + "Hello. How have you been? Working for \"Cam-Corp\" is still fun. It has been great here. People are very nice here and I have fit in really good in here.\n"
+                + "Although, I have a feeling something else is happening here.. People keep going into this inclosed room next to my table. I can not see what it is but\n"
+                + "I keep seeing bright lights come into the room at different times... Weird lights. Ones I have never seen before... Anyways I hope you are \n"
+                + "having fun! I will see you soon. Love, " + p.getName());
         promptEnterKey();
         clear();
-        System.out.println("Year: 2019\n" +
-                           "Location: Cam Coorperation\n" +
-                           "Date: January 10th\n" +
-                           "\n" +
-                           "Hello.. It has been awile. I don't even know where I am now. It's dark... All I can see is the awkward lights that is coming from the room. Someone is \n" +
-                           "coming out.. He is wearing a black uniform.. kind of like a ninja. My brain feels like it's trying to come out of my head. I can feel my heartbeat and\n" +
-                           "it's driving me crazy. The man is walking towards me, his eyes are glowing!! Where am I.. What do they wa-" );
+        System.out.println("Year: 2019\n"
+                + "Location: Cam Coorperation\n"
+                + "Date: January 10th\n"
+                + "\n"
+                + "Hello.. It has been awile. I don't even know where I am now. It's dark... All I can see is the awkward lights that is coming from the room. Someone is \n"
+                + "coming out.. He is wearing a black uniform.. kind of like a ninja. My brain feels like it's trying to come out of my head. I can feel my heartbeat and\n"
+                + "it's driving me crazy. The man is walking towards me, his eyes are glowing!! Where am I.. What do they wa-");
         System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------- ");
-    }
-    
-    public static void promptEnterKey(){
+    }//End of Dialogue
+
+    public static void promptEnterKey() {
         System.out.println("Press \"ENTER\" to continue...");
         try {
             int read = System.in.read(new byte[2]);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-}
+    }//End of promptEnterKey
+
+    
+     public static void move(Scanner in, int[][] theMap, Player P, TimeZone tz){
+        int dir = 0;
+        int choice = 0;
+        
+        System.out.println("What direction would you want to go?: "
+                + "1)N, 2)E, 3)S, 4)W");
+        dir = in.nextInt();
+        
+        switch(dir){
+            case 1://North
+                if(theMap[P.getYPos() -1][ P.getXPos()]==4) {
+                    System.out.println("There's a wall.. you can't go there.");
+                }
+                else {
+                    P.setYPos(P.getYPos() - 1);
+                    if(theMap[P.getYPos()][P.getXPos()]==8){
+                        System.out.println("Would you like to timetravel?: "
+                                + "\n1)Yes"
+                                + "\n2)No");
+                        choice = in.nextInt();
+                        
+                        if(choice ==   1)
+                            tz.timeTravel(in);
+                    }
+                }
+                break;
+            case 2://East
+                if(theMap[P.getYPos()][ P.getXPos() + 1]==4) {
+                    System.out.println("There's a wall.. you can't go there.");
+                }
+                else {
+                    P.setXPos(P.getXPos() + 1);
+                    if(theMap[P.getYPos()][P.getXPos()]==8){
+                        System.out.println("Would you like to timetravel?: "
+                                + "\n1)Yes"
+                                + "\n2)No");
+                        choice = in.nextInt();
+                        
+                        if(choice ==   1)
+                            tz.timeTravel(in);
+                    }
+                }
+                break;
+            case 3://South
+                if(theMap[P.getYPos() +1][ P.getXPos()]==4) {
+                    System.out.println("There's a wall.. you can't go there.");
+                }
+                else {
+                    P.setYPos(P.getYPos() + 1);
+                    if(theMap[P.getYPos()][P.getXPos()]==8){
+                        System.out.println("Would you like to timetravel?: "
+                                + "\n1)Yes"
+                                + "\n2)No");
+                        choice = in.nextInt();
+                        
+                        if(choice ==   1)
+                            tz.timeTravel(in);
+                    }
+                }
+                break;
+            case 4://West
+                if(theMap[P.getYPos() ][ P.getXPos() - 1]==4) {
+                    System.out.println("There's a wall.. you can't go there.");
+                }
+                else {
+                    P.setXPos(P.getXPos() - 1);
+                    if(theMap[P.getYPos()][P.getXPos()]==8){
+                        System.out.println("Would you like to timetravel?: "
+                                + "\n1)Yes"
+                                + "\n2)No");
+                        choice = in.nextInt();
+                        
+                        if(choice ==   1)
+                            tz.timeTravel(in);
+                    }
+                }
+                break;
+                
+        }
+    }// end of movefunction
+     
+}//End of Driver
 
